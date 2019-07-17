82f0484c0ca6919a531b9639ba225cb78eeb907f
berry_cooper <berry_cooepr@163.com>
2019-07-17 10:28:22 +0800

    授权验证通过

--- src/test/java/com/berry/StorageTest.java
+++ src/test/java/com/berry/StorageTest.java
@@ -92,7 +92,7 @@ public class StorageTest {
      */
     @Test
     public void getObjectTest() throws IOException {
-        byte[] data = objectManage.getObject("test", "main_bg.png");
+        byte[] data = objectManage.getObject("test", "NotFound-260-260.png");
         if (data != null) {
             FileOutputStream outputStream = new FileOutputStream(new File("./test2.png"));
             outputStream.write(data);
